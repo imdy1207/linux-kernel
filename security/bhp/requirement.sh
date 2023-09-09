357844bdd35d28fb089a3107a1f787d6b75384ba
imdy1207 <imdy1207@gmail.com>
2023-09-08 20:13:18 -0900

    add cmm script, python sniffing tool

---
+++ security/bhp/requirement.sh
@@ -0,0 +1,19 @@
+# set envirenment
+sudo apt update
+apt list --upgradeable
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt autoremove
+
+# install python3.6~
+sudo apt-get upgrade python3
+sudo apt-get -y install python3-venv
+python3 -m venv venv3
+#source venv3/bin/activate
+
+# install IDE
+sudo apt-get -y install code
+# installed specified version
+#sudo apt-get install -f ./code_1.56.0-1620166262_amd64.deb
+
+
